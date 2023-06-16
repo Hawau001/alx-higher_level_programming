0307c69b71ccd9fc178d8206410006d637a1a12b
Hawau <117785659+Hawau001@users.noreply.github.com>
2023-06-16 03:13:53 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-let text;
-if (process.argv[2] !== undefined) {
-	  text = process.argv[2];
-} else {
-	  text = 'undefined';
-}
-console.log(text.concat(' is ', process.argv[3]));
--- 0x12-javascript-warm_up/7-multi_c.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-let x = process.argv[2];
-if (isNaN(parseInt(x))) {
-	  console.log('Missing number of occurrences');
-} else {
-	  while (x-- > 0) {
-		      console.log('C is fun');
-		    }
-}
